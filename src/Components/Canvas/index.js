eb3be947ea3257493199a8859052cdf27a50b876
Christian <k.christian@dickblick.com>
2022-02-27 00:37:51 -0600

    restoring functionality

--- src/Components/Canvas/index.js
+++ src/Components/Canvas/index.js
@@ -340,6 +340,7 @@ const Canvas = () => {
 
   return (
     <div>
+      <Styled.Title>shape toy prompt</Styled.Title>
       <Styled.ButtonPane>
         <Styled.ControlButtons
           data-testid="draw-rectangle-button"
@@ -406,22 +407,107 @@ const Canvas = () => {
         `}
       >
         {showRect && (
-          <ShapeControlRect
-            rectLocation={rectLocation}
-            clearCanvas={clearCanvas}
-            drawRectangle={drawRectangle}
-            drawCircle={drawCircle}
-            deleteRect={deleteRect}
-          />
+          <div
+            className={css`
+              display: flex;
+              flex-direction: column;
+            `}
+          >
+            <h2>Rectangle location</h2>X location: {rectLocation.x}Y location"{" "}
+            {rectLocation.y}
+            <label htmlFor="height">Height: {rectLocation.h}px</label>
+            <input
+              type="range"
+              id="rect-height"
+              name="height"
+              min="30"
+              max="300"
+              value={rectLocation.h}
+              onChange={(e) => {
+                clearCanvas();
+                drawRectangle({ ...rectLocation, h: e.target.value }, 1);
+                if (circLocation) {
+                  drawCircle(circLocation);
+                }
+              }}
+            />
+            <label htmlFor="width">Width: {rectLocation.w}px</label>
+            <input
+              type="range"
+              id="rect-width"
+              name="width"
+              min="30"
+              max="300"
+              value={rectLocation.w}
+              onChange={(e) => {
+                clearCanvas();
+                drawRectangle({ ...rectLocation, w: e.target.value }, 1);
+                if (circLocation) {
+                  drawCircle(circLocation);
+                }
+              }}
+            />
+            <label htmlFor="rect-color">Color</label>
+            <input
+              type="color"
+              id="rect-color"
+              name="rect-color"
+              value={rectLocation.c}
+              onChange={(e) => {
+                clearCanvas();
+                drawRectangle({ ...rectLocation, c: e.target.value }, 1);
+                if (circLocation) {
+                  drawCircle(circLocation);
+                }
+              }}
+            />
+            <button type="button" data-testid="clear-rect" onClick={deleteRect}>
+              delete
+            </button>
+          </div>
         )}
         {showCirc && (
-          <ShapeControlCirc
-            circLocation={circLocation}
-            clearCanvas={clearCanvas}
-            drawRectangle={drawRectangle}
-            drawCircle={drawCircle}
-            deleteCirc={deleteCirc}
+          <div
+          className={css`
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <h2>Circle location</h2>X location: {circLocation.x}Y location:{" "}
+          {circLocation.y}
+          <label htmlFor="width">Radius: {circLocation.r}px</label>
+          <input
+            type="range"
+            id="radius"
+            name="radius"
+            min="15"
+            max="150"
+            value={circLocation.r}
+            onChange={(e) => {
+              clearCanvas();
+              drawCircle({ ...circLocation, r: e.target.value }, 1);
+              if (rectLocation) {
+                drawRectangle(rectLocation);
+              }
+            }}
           />
+          <input
+            type="color"
+            id="circ-color"
+            name="circ-color"
+            value={circLocation.c}
+            onChange={(e) => {
+              clearCanvas();
+              drawCircle({ ...circLocation, c: e.target.value }, 1);
+              if (rectLocation) {
+                drawRectangle(rectLocation);
+              }
+            }}
+          />
+          <button type="button" data-testid="clear-circ" onClick={deleteCirc}>
+            delete
+          </button>
+        </div>
         )}
       </div>
     </div>
